79913d728fee18f86876454c0449a37721fbf72b
Gabriel Lu <GLu@Gabriels-MacBook-Pro.local>
2015-12-28 14:47:47 -0500

    Nav

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,29 @@
 (function($) {
 
+	$('a[href*=#]').bind("click", function(e){
+           
+			var anchor = $(this);
+			$('html, body').stop().animate({
+				scrollTop: $(anchor.attr('href')).offset().top
+			}, 1000);
+			e.preventDefault();
+		});
+
+		$(window).scroll(function() {
+			if ($(this).scrollTop() > 100) {
+				$('.scroll-up').fadeIn();
+			} else {
+				$('.scroll-up').fadeOut();
+			}
+		});
+
+
 $(document).ready(function () {
+ wow = new WOW({
+			mobile: false
+		});
+		wow.init();
+
   $(".screen-height").height($(window).height());
 
 		$(window).resize(function(){
